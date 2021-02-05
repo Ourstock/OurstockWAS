4dd6800fb52ae96c20afb437e1547115080d8801
pjkfckr <pjkfckr@gmail.com>
2021-02-05 14:19:32 +0900

    [Add]Change User Table name

--- src/main/java/com/outstock/api/outstock_api/model/UserEntity.java
+++ src/main/java/com/outstock/api/outstock_api/model/UserEntity.java
@@ -13,7 +13,7 @@ import java.util.Date;
 
 @JsonIgnoreProperties({"hibernateLazyInitializer"})
 @Entity
-@Table(name = "user")
+@Table(name = "users")
 public class UserEntity implements Serializable {
 
     @Id
