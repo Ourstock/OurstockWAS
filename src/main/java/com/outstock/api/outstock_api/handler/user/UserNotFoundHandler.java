13a49d721ed2ab92a77011ad4f3e658196d6ddf8
pjkfckr <pjkfckr@gmail.com>
2021-02-09 15:42:24 +0900

    [Add] Fcm, Channel 모델추가 및 Api 추가

---
+++ src/main/java/com/outstock/api/outstock_api/handler/user/UserNotFoundHandler.java
@@ -0,0 +1,8 @@
+package com.outstock.api.outstock_api.handler.user;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "Not Exist User")
+public class UserNotFoundHandler extends RuntimeException {
+}
--- src/main/java/com/outstock/api/outstock_api/service/UserService.java
+++ src/main/java/com/outstock/api/outstock_api/service/UserService.java
@@ -1,12 +1,17 @@
 package com.outstock.api.outstock_api.service;
 
 
+import com.outstock.api.outstock_api.dto.user.UserFcmTokenDto;
 import com.outstock.api.outstock_api.dto.user.UserLoginDto;
 import com.outstock.api.outstock_api.dto.user.UserSignUpDto;
+import com.outstock.api.outstock_api.handler.user.JwtTokenHandler;
 import com.outstock.api.outstock_api.handler.user.UserLoginHandler;
+import com.outstock.api.outstock_api.handler.user.UserNotFoundHandler;
 import com.outstock.api.outstock_api.handler.user.UserSignUpHandler;
 import com.outstock.api.outstock_api.jwt.JwtToken;
+import com.outstock.api.outstock_api.model.Fcm;
 import com.outstock.api.outstock_api.model.UserEntity;
+import com.outstock.api.outstock_api.repository.FcmRepository;
 import com.outstock.api.outstock_api.repository.UserRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,13 +21,16 @@ import org.springframework.transaction.annotation.Transactional;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final FcmRepository fcmRepository;
     private final JwtToken jwtToken;
 
     public UserService(
             UserRepository userRepository,
+            FcmRepository fcmRepository,
             JwtToken jwtToken
     ) {
         this.userRepository = userRepository;
+        this.fcmRepository = fcmRepository;
         this.jwtToken = jwtToken;
     }
 
@@ -43,17 +51,36 @@ public class UserService {
                 .build();
         return userRepository.save(userEntity);
     }
+
 //    로그인
     @Transactional
     public UserEntity login(UserLoginDto userLoginDto) {
-        if (userRepository.findOneByCallNumber(userLoginDto.getCallNumber()).orElse(null) == null) {
+        if (userRepository.findOneByCallNumber(userLoginDto.getCallNumber()).isEmpty()) {
             throw new UserLoginHandler();
         }
-
         UserEntity userEntity = userRepository.findByCallNumber(userLoginDto.getCallNumber());
         String token = jwtToken.createToken(userLoginDto.getCallNumber());
         userEntity.setJwtToken(token);
         return userRepository.save(userEntity);
     }
 
+    @Transactional
+    public boolean validJwtToken(long userId, String jwtToken) {
+        return userRepository.getOne(userId).getJwtToken().equals(jwtToken);
+    }
+
+    @Transactional
+    public Fcm saveUserFcmToken(UserFcmTokenDto userFcmTokenDto, String jwtToken) {
+        if (userRepository.findById(userFcmTokenDto.getUserId()).isPresent()) {
+            if (validJwtToken(userFcmTokenDto.getUserId(),jwtToken)) {
+                Fcm fcm = Fcm.builder()
+                        .fcmToken(userFcmTokenDto.getFcmToken())
+                        .userId(userFcmTokenDto.getUserId())
+                        .build();
+                return fcmRepository.save(fcm);
+            }
+            throw new JwtTokenHandler();
+        }
+        throw new UserNotFoundHandler();
+    }
 }
