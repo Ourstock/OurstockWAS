15592f2467b6682dc172d89ec748798910a21464
pjkfckr <pjkfckr@gmail.com>
2021-02-22 16:56:34 +0900

    [Add] Mysql Connection Time out

--- src/main/java/com/ourstock/api/ourstock_api/dto/order/OrderUpdateDto.java
+++ src/main/java/com/ourstock/api/ourstock_api/dto/order/OrderUpdateDto.java
@@ -13,8 +13,24 @@ public class OrderUpdateDto {
     @NotNull
     private long userId;
 
-    public OrderUpdateDto(@NotNull long orderId, @NotNull long userId) {
+    @NotNull
+    private String channelId;
+
+    @NotNull
+    private long orderCount;
+
+    @NotNull
+    private long orderCost;
+
+    @NotNull
+    private String type;
+
+    public OrderUpdateDto(@NotNull long orderId, @NotNull long userId, @NotNull String channelId, @NotNull long orderCount, @NotNull long orderCost, @NotNull String type) {
         this.orderId = orderId;
         this.userId = userId;
+        this.channelId = channelId;
+        this.orderCount = orderCount;
+        this.orderCost = orderCost;
+        this.type = type;
     }
 }
